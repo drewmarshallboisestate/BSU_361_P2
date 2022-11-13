5552e8f5a947065e3a8e9d3e8bd9433da7ad269a
Drew Marshall <drewmarshall@u.boisestate.edu>
2022-11-13 00:27:41 -0700

    Worked on NFAState.java

--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -1,6 +1,7 @@
 package fa.nfa;
 import fa.State;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -11,17 +12,17 @@ public class NFAState extends State {
     
 
     // Create a linked HashMap to represent the NFA transitions 
-    private LinkedHashMap<Character, Set<NFAState>> NFATransitionMap;
+    private LinkedHashMap<Character, HashSet<NFAState>> NFATransitionMap;
     // Need flag to keep track of final states
     private boolean finalStateFlag;
 
-    public NFAState(String stateName) {
+    public NFAState(String stateName) { // Constructor for NFAState
         // Set the name passed in from state to create the new NFAState
         this.name = name;
         // Create the transition map for the new name 
-        NFATransitionMap = new LinkedHashMap<Character, Set<NFAState>>();
+        NFATransitionMap = new LinkedHashMap<Character, HashSet<NFAState>>();
         // Initialize final state flag to false 
-        finalStateFlag = false;
+        this.finalStateFlag = false;
     }
 
     public void addTransition(char onSymb, NFAState toState) {
@@ -31,7 +32,7 @@ public class NFAState extends State {
             NFATransitionMap.get(onSymb).add(toState);
         } else {
             // Otherwise we need to initalize the new onSymb and add the transition
-            Set<NFAState> onSymbSet = new LinkedHashSet<>();
+            HashSet<NFAState> onSymbSet = new HashSet<NFAState>();
             // Add the state to transition to based on the input alphabet symbol
             onSymbSet.add(toState);
             // Finally add this set to the transition map 
@@ -39,7 +40,7 @@ public class NFAState extends State {
         }
     }
 
-    public Set<NFAState> getStateOnSymb(char onSymb) {
+    public Set<NFAState> getStateOnSymb(char onSymb) { // Get the next transition 
         // Check if the transition table contains the character symbol 
         if (NFATransitionMap.containsKey(onSymb)) {
             // If there is a transition return the state set that can be transitioned to
@@ -50,17 +51,21 @@ public class NFAState extends State {
         }
     }
 
-    public NFAState(String name, boolean finalFlag) {
+    public NFAState(String name, boolean finalFlag) { // Constructor for final state
         // Set final flag to be set to boolean passed in 
         this.finalStateFlag = finalFlag;
         // Set the name passed in from state to create the new NFAState
         this.name = name;
         // Create the transition map for the new name 
-        NFATransitionMap = new LinkedHashMap<Character, Set<NFAState>>();
+        NFATransitionMap = new LinkedHashMap<Character, HashSet<NFAState>>();
     }
 
-    public boolean isFinalFlag() {
+    public boolean isFinalFlag() {//
         // Return if the NFAState is a final state or not
         return finalStateFlag;
+    }
+
+    public LinkedHashMap<Character, HashSet<NFAState>> getTransitions() {
+        return NFATransitionMap;
     }
 }
